052a995a7bd6da3d6e57ab9750face80276abbd7
t8t8t8 <shaw@Shaw.local>
2014-12-16 20:01:41 -0500

    geese added (faulty)

--- src/tron/SinglePlayer.java
+++ src/tron/SinglePlayer.java
@@ -1,7 +1,17 @@
 package tron;
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.geom.AffineTransform;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
 /*
  * This is the one player version
@@ -13,25 +23,61 @@ import java.awt.event.*;
 
 public class SinglePlayer extends JFrame{
 
-	final int NORTH = 1;
-	final int EAST = 2;
-	final int SOUTH = 3;
-	final int WEST = 4;
+	public static enum Direction{
+		NORTH, EAST, SOUTH, WEST
+	}
 	
 	final int WIDTH = 100;
 	final int HEIGHT = 100;
+
 	final int PPI = 4; 		//Pixel density
+	final int TICK = 100;	//Timer speed
 	
-	final Color color = Color.BLUE;
+	final static int PORT = 8002;			//The network port the game uses
 
-	private int dir;
+	final int NUM_PLAYERS = 2;		//Number of players
+	
+	//Colors for each player, and dead blocks
+	final Color[] color = {Color.BLUE, Color.RED, Color.BLACK};
+	
+	//Communications announcement codes
+	public static enum Code{
+		SEND_START, SEND_DIR, SEND_ARR, SEND_LOSS, SEND_LOC
+	}
+//	final int SEND_START = 10;	//Game starting
+//	final int SEND_DIR = 11;	//Send new direction
+//	final int SEND_ARR = 12;	//Send board
+//	final int SEND_LOSS = 13;	//A player lost
+//	final int SEND_LOC = 14; 	//Send location
+	
+	private int SELF;		//Which player this is
+	
+	
+	
+	
+//	private byte[][] grid;
+	
+	private Timer timer;		//A timer for when the server's slow
+	
+	private KeyboardPanel kp;	//Player interacts with keyboard
+
+	private JButton btnNew;		//Button used to start the game
+	private JLabel lblStatus;	//Label to display winners for status messages
+	
+	//**************************************
+	//Direction and location of the players
+	private Direction dir;
 	private int locX,locY;
+	
+	//Game grid
 	private boolean[][] grid;
-	private Timer timer;
-	private KeyboardPanel kp;
-	private JButton btnNew;
 
+	private Image[] goose;
+	
 	public static void main (String args[]){
+
+		
+		
 		new SinglePlayer();
 
 	}
@@ -40,8 +86,8 @@ public class SinglePlayer extends JFrame{
 
 
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setTitle("Tron");
-		this.setResizable(false);
+		this.setTitle("Honk Honk");
+//		this.setResizable(false);
 
 		
 		JPanel pMenu = new JPanel();
@@ -66,19 +112,33 @@ public class SinglePlayer extends JFrame{
 //		grid = new boolean[WIDTH][HEIGHT];
 
 
-		this.setVisible(true);		
-		System.out.println(this.getInsets());
+		this.setVisible(true);
 		this.setSize(PPI*WIDTH,PPI*HEIGHT+this.getInsets().top+pMenu.getHeight());
 
+
+		grid = new boolean[WIDTH][HEIGHT];
+		loadImage();
 	}
+	
+	private void loadImage(){
+		
+		goose = new Image[4];
+		goose[0] = Toolkit.getDefaultToolkit().createImage("rsc/flying-goose-up.gif");
+		goose[1] = Toolkit.getDefaultToolkit().createImage("rsc/flying-goose-right.gif");
+		goose[2] = Toolkit.getDefaultToolkit().createImage("rsc/flying-goose-down.gif");
+		goose[3] = Toolkit.getDefaultToolkit().createImage("rsc/flying-goose-left.gif");
+
+
+	}
+	
 	
 	private void newGame(){
 		locX = WIDTH-3;
 		locY = HEIGHT /2;
-		dir = WEST;
+		dir = Direction.WEST;
 		grid = new boolean[WIDTH][HEIGHT];
-		repaint();
 		timer.start();
+		repaint();
 	}
 	
 	class TimerListener implements ActionListener {
@@ -121,17 +181,17 @@ public class SinglePlayer extends JFrame{
 				public void keyPressed(KeyEvent e){
 					switch (e.getKeyCode()){
 					case KeyEvent.VK_UP: 
-						dir = NORTH;
+						dir = Direction.NORTH;
 						System.out.println("Up key");
 						break;
 					case KeyEvent.VK_DOWN: 
-						dir = SOUTH;
+						dir = Direction.SOUTH;
 						break;
 					case KeyEvent.VK_LEFT: 
-						dir = WEST;
+						dir = Direction.WEST;
 						break;
 					case KeyEvent.VK_RIGHT: 
-						dir = EAST;
+						dir = Direction.EAST;
 						break;
 					default:
 						System.out.println("Invalid key");
@@ -142,8 +202,44 @@ public class SinglePlayer extends JFrame{
 		}
 		
 		protected void paintComponent(Graphics g){
-			g.setColor(color);
+			
+			if (!timer.isRunning()){
+				return;
+			}
+			
+			g.setColor(color[2]);
+			for (int i = 0; i < grid.length; i++) {
+				for (int j = 0; j < grid[0].length; j++) {
+					if (grid[i][j]){
+						g.fillRect(i*PPI, j*PPI, PPI, PPI);
+					}
+				}
+			}
+			
+			g.setColor(color[0]);
 			g.fillRect(locX*PPI, locY*PPI, PPI, PPI);
+			
+			int direction=0;
+			switch (dir){
+			case NORTH: direction = 0;
+				break;
+			case EAST: direction = 1;
+				break;
+			case SOUTH: direction = 2;
+				break;
+			case WEST: direction = 3;
+				break;
+			}
+			
+            Graphics2D g2d = (Graphics2D) g.create();
+            AffineTransform at = new AffineTransform();
+            at.setToRotation(Math.PI/2*direction, locX*PPI, locY*PPI);
+//            g2d.setTransform(at);
+//            g2d.rotate(Math.PI/2);
+            g2d.drawImage(goose[direction], at, null);
+            g2d.dispose();
+            
+			//g.drawImage(goose, locX*PPI, locY*PPI, 5*PPI, 5*PPI, null);
 			
 		}
 		
